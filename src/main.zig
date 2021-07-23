f68aaf6372445d49589e9db8c99bd6600feca004
Linfeng Qian <thewawar@gmail.com>
2021-07-23 21:13:35 +0800

    Update

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,16 @@
+
 const std = @import("std");
 
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+export fn _start() callconv(.Naked) noreturn {
+// pub fn main() void {
+    var i: usize = 1;
+    var acc: usize = 0;
+    while (true) {
+        acc +%= i;
+        if (i > 6_0000_0000) {
+            break;
+        }
+        i += 1;
+    }
+    unreachable;
 }
