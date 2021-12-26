e1f0890fd80ecc36658b40fb5cffba12296055ba
Linfeng Qian <thewawar@gmail.com>
2021-12-26 13:59:48 +0800

    Set ckb-std build target

--- ckb-std/build.zig
+++ ckb-std/build.zig
@@ -1,12 +1,45 @@
 const std = @import("std");
 
+const builtin = std.builtin;
+const Target = std.Target;
+const RiscvFeature = Target.riscv.Feature;
+
 pub fn build(b: *std.build.Builder) void {
+    var cpu_features_add = Target.Cpu.Feature.Set.empty;
+    var cpu_features_sub = Target.Cpu.Feature.Set.empty;
+    inline for (.{
+        RiscvFeature.@"64bit",
+        RiscvFeature.c,
+        RiscvFeature.m,
+    }) |feature| {
+        cpu_features_add.addFeature(@enumToInt(feature));
+    }
+    inline for (.{
+        RiscvFeature.a,
+        RiscvFeature.d,
+        RiscvFeature.e,
+        RiscvFeature.f,
+        // TODO: will be supported in future update of zig
+        RiscvFeature.experimental_b,
+        RiscvFeature.experimental_v,
+    }) |feature| {
+        cpu_features_sub.addFeature(@enumToInt(feature));
+    }
+    const target = std.zig.CrossTarget{
+        .cpu_arch = .riscv64,
+        .os_tag = .freestanding,
+        .cpu_features_add = cpu_features_add,
+        .cpu_features_sub = cpu_features_sub,
+        .abi = .none,
+    };
+
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
     const lib = b.addStaticLibrary("ckb-std", "src/ckb_std.zig");
     lib.addAssemblyFile("src/syscall.S");
+    lib.setTarget(target);
     lib.setBuildMode(mode);
     lib.install();
 
